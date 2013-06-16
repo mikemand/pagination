693e9f5506b2cb928ece9822512fe3bfcb479d5c
Micheal <micheal@kmdwebdesigns.com>
2013-06-16 13:34:40 -0500

    Fix config for slider's classes

--- src/views/slider.php
+++ src/views/slider.php
@@ -3,7 +3,7 @@
 ?>
 
 <?php if (Config::get('pagination::always_show') OR $paginator->getLastPage() > 1): ?>
-    <div class="<?php echo Config::get('pagination::classes.slider') ?>">
+    <div class="<?php echo Config::get('pagination::classes') ?>">
         <ul>
             <?php echo $presenter->render(); ?>
         </ul>
